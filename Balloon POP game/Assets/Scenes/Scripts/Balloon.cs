e60aa654669e8a30e011f5346c13e45eb7098ce2
Victoria <Victoria.b930@stu.nebo.edu>
2022-09-14 09:20:46 -0600

    Added Balloon and Balloon Script

---
+++ Balloon POP game/Assets/Scenes/Scripts/Balloon.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balloon : MonoBehaviour
+{
+    public int clickToPop = 3; //How many clicks before balloon pops
+    public float scaleToIncrease = 0.10f; //Scale to increase each time te baloon is clicked (inflating)
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnMouseDown ()
+    {
+        //Reduce clicks by one
+        clickToPop -= 1;
+
+        //Increase balloon volume
+        transform.localScale += Vector3.one * scaleToIncrease;
+
+        if(clickToPop == 0)
+        {
+            //scoreManager.IncreaseScoreText(scoreToGive); 
+            Destroy(gameObject);
+        }
+    }
+
+}
