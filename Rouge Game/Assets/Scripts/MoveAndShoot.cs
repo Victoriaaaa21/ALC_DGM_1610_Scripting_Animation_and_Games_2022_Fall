a923430bb3ffa76f13b759ea8747a1cfed85f99f
Victoria <Victoria.b930@stu.nebo.edu>
2022-10-20 09:21:27 -0600

    Finishing Enemy Attacks and Start of Player Attack

--- Rouge Game/Assets/Scripts/MoveAndShoot.cs
+++ Rouge Game/Assets/Scripts/MoveAndShoot.cs
@@ -16,12 +16,38 @@ public class MoveAndShoot : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+       target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+       shotDelay = startDelay;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+       if(Vector2.Distance(transform.position, target.position) > stopDistance)
+       {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed *Time.deltaTime); //Moves towards target
+       } 
+        else if(Vector2.Distance(transform.position, target.position) < stopDistance && Vector2.Distance(transform.position, target.position) > retreatDistance)
+        {
+            transform.position = this.transform.position; //Stay put
+        }
+        else if(Vector2.Distance(transform.position, target.position) < retreatDistance)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, target.position, -speed * Time.deltaTime); //Run Away
+        }
+
+
+
+        //Ranged Attack
+        if(shotDelay <=0)
+        {
+            Instantiate(projectile, transform.position, Quaternion.identity); //Fire Projectile
+            shotDelay = startDelay;
+        }
+        else
+        {
+            shotDelay -= Time.deltaTime; //Counting down
+        }
     }
 }
---
+++ Rouge Game/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    private float attackDelay;
+    public float startDelay;
+    public Transform attackPos;
+    public LayerMask whatIsEnemies;
+    public float attackRange;
+    public int damage; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(attackDelay <= 0)
+        {
+            if(Input.GetKey(KeyCode.Space))
+            {
+                Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPos.position, attackRange, whatIsEnemies);
+
+                for(int i = 0; i < enemiesToDamage.Length; i++)
+                {
+                    enemiesToDamage[i].GetComponent<Enemy>().TakeDamage(damage);
+                }
+            }
+
+            attackDelay = startDelay; //Restarts the delay
+        }
+        else
+        {
+            attackDelay -= Time.deltaTime; //Count Down
+        }
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
